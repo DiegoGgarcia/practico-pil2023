ec879b0061f9f12b3049303f1b64382230138e4f
diegogarcia2023 <diego_garcia@live.com.ar>
2023-08-29 19:27:29 -0300

    Cambio de match por elif

--- modules/apis/lugares.py
+++ modules/apis/lugares.py
@@ -29,46 +29,43 @@ class LugaresResource(Resource):
 				lugares_data.append(pd)
 			return {"Exito":True,"MensajePorFallo":None,"Resultado":lugares_data}, 200
 		
+		elif (lugar_type=='obtener_paises'):
+			paises = gestor_lugares().consultar_paises()
+			paises_data = [pais.serialize() for pais in paises]
+			return {"Exito":True,"MensajePorFallo":None,"Resultado":paises_data}, 200
 		else:
-			match lugar_type:
-				case 'obtener_paises':
-					paises = gestor_lugares().consultar_paises()
-					paises_data = [pais.serialize() for pais in paises]
-					return {"Exito":True,"MensajePorFallo":None,"Resultado":paises_data}, 200
-				case _:
-					return {"Exito":False,"MensajePorFallo":"Recurso no definido","Resultado":None}, 400
+			return {"Exito":False,"MensajePorFallo":"Recurso no definido","Resultado":None}, 400
 
 	@jwt_or_login_required()
 	def post(self, lugar_type=None):
-		match lugar_type:
-			case 'obtener_provincias':
-				data = request.get_json() 
-				pais = data.get('pais')
-				if not pais:
-					return {"Exito":False,"MensajePorFallo":"Debe indicar el pais","Resultado":None}, 400
-				provincias = gestor_lugares().consultar_provincias(pais=pais)
-				provincias_data = [provincia.serialize() for provincia in provincias]
-				return {"Exito":True,"MensajePorFallo":None,"Resultado":provincias_data}, 200
-			case 'obtener_ciudades':
-				data = request.get_json() 
-				pais = data.get('pais')
-				provincia = data.get('provincia')
-				if not pais or not provincia:
-					return {"Exito":False,"MensajePorFallo":"Debe indicar el pais y provincia","Resultado":None}, 400
-				ciudades = gestor_lugares().consultar_ciudades(pais=pais, provincia=provincia)
-				ciudades_data = [ciudad.serialize() for ciudad in ciudades]
-				return {"Exito":True,"MensajePorFallo":None,"Resultado":ciudades_data}, 200
-			case 'obtener_barrios':
-				data = request.get_json() 
-				pais = data.get('pais')
-				provincia = data.get('provincia')
-				ciudad = data.get('ciudad')
-				if not pais or not provincia or not ciudad:
-					return {"Exito":False,"MensajePorFallo":"Debe indicar el pais, provincia y ciudad","Resultado":None}, 400
-				barrios = gestor_lugares().consultar_barrios(pais=pais, provincia=provincia, ciudad=ciudad)
-				barrios_data = [barrio.serialize() for barrio in barrios]
-				return {"Exito":True,"MensajePorFallo":None,"Resultado":barrios_data}, 200
-			case _:
-				return {"Exito":False,"MensajePorFallo":"Recurso no definido","Resultado":None}, 400
+		if (lugar_type == 'obtener_provincias'):
+			data = request.get_json() 
+			pais = data.get('pais')
+			if not pais:
+				return {"Exito":False,"MensajePorFallo":"Debe indicar el pais","Resultado":None}, 400
+			provincias = gestor_lugares().consultar_provincias(pais=pais)
+			provincias_data = [provincia.serialize() for provincia in provincias]
+			return {"Exito":True,"MensajePorFallo":None,"Resultado":provincias_data}, 200
+		elif (lugar_type ==  'obtener_ciudades'):
+			data = request.get_json() 
+			pais = data.get('pais')
+			provincia = data.get('provincia')
+			if not pais or not provincia:
+				return {"Exito":False,"MensajePorFallo":"Debe indicar el pais y provincia","Resultado":None}, 400
+			ciudades = gestor_lugares().consultar_ciudades(pais=pais, provincia=provincia)
+			ciudades_data = [ciudad.serialize() for ciudad in ciudades]
+			return {"Exito":True,"MensajePorFallo":None,"Resultado":ciudades_data}, 200
+		elif (lugar_type == 'obtener_barrios'):
+			data = request.get_json() 
+			pais = data.get('pais')
+			provincia = data.get('provincia')
+			ciudad = data.get('ciudad')
+			if not pais or not provincia or not ciudad:
+				return {"Exito":False,"MensajePorFallo":"Debe indicar el pais, provincia y ciudad","Resultado":None}, 400
+			barrios = gestor_lugares().consultar_barrios(pais=pais, provincia=provincia, ciudad=ciudad)
+			barrios_data = [barrio.serialize() for barrio in barrios]
+			return {"Exito":True,"MensajePorFallo":None,"Resultado":barrios_data}, 200
+		else:
+			return {"Exito":False,"MensajePorFallo":"Recurso no definido","Resultado":None}, 400
 
 	
